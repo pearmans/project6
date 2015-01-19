fca836f48759a9c6b1f97e6cd448dac04412de45
Stuart Pearman <stuart@element84.com>
2015-01-19 15:40:19 -0500

    routed "/" to views/index.html

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require 'sinatra'
 
 class MyWebbApp < Sinatra::Base
 	get '/' do
-		"Welcome to the webapp of DOOM, where the copyright is always up to date! Copyright #{ Time.now.year} Stuart Pearman."
+		send_file 'views/index.html'
 	end
 end 
