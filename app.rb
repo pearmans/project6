37cabf6b082334a297aef31c39550cda7590fadc
Stuart Pearman <stuart@element84.com>
2015-01-27 00:18:38 -0500

    most profitable app idea ever

--- app.rb
+++ app.rb
@@ -2,6 +2,14 @@ require 'sinatra'
 
 class MyWebbApp < Sinatra::Base
 	get '/' do
-		send_file "views/index#{ rand(3) + 1 }.html"
+		send_file "views/index1.html"
+	end
+
+	get '/suspense/' do
+		send_file "views/index2.html"
+	end
+
+	get '/finale/' do
+		send_file "views/index3.html"
 	end
 end 
