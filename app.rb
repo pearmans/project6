1c51698ff3688725a4bf8b85f59aef8e1ef6c74b
Stuart Pearman <stuart@element84.com>
2015-02-16 11:43:26 -0500

    Killed DS_Store 4ever

--- app.rb
+++ app.rb
@@ -2,14 +2,6 @@ require 'sinatra'
 
 class MyWebbApp < Sinatra::Base
 	get '/' do
-		erb :index1
-	end
-
-	get '/page2/' do
-		erb :index2
-	end
-
-	get '/page3/' do
-		erb :index3
+		erb :index
 	end
 end 
