31535926cb7ce19839f628648a81cc64cc3c7067
Stuart Pearman <stuart@element84.com>
2015-02-22 13:20:01 -0500

    finalized cat technology and holidays

--- app.rb
+++ app.rb
@@ -1,7 +1,16 @@
 require 'sinatra'
+require 'cat_api'
+require 'holidapi'
 
 class MyWebbApp < Sinatra::Base
 	get '/' do
+		@holidays_dob = HolidApi.get(year: 1993, month: 8)
+		@holidays_m = HolidApi.get(year: Time.now.year, month: Time.now.month)
 		erb :index
+	end
+
+	get '/cats/' do
+		@pictures = CatAPI.new.get_images(category: 'hats')
+		erb :cats
 	end
 end 
