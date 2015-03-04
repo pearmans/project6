fedd5669f2841be0b3db6ce08f97cce0ace57631
Stuart Pearman <stuart@element84.com>
2015-03-04 18:01:54 -0500

    soft tabs to app.rb

--- app.rb
+++ app.rb
@@ -3,18 +3,18 @@ require 'cat_api'
 require 'holidapi'
 
 class MyWebbApp < Sinatra::Base
-	get '/' do
-		params['year'] = Time.now.year unless params['year']
-		params['month'] = Time.now.month unless params['month']
-		params['country'] = 'US' unless params['country']
-		@months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-		@countries = [['US','United States'], ['BE','Belgium'], ['BR','Brazil'], ['GB','Great Britain']]
-		@holidays_user = HolidApi.get(year: params['year'], month: params['month'], country: params['country'])
-		erb :index
-	end
+  get '/' do
+    params['year'] = Time.now.year unless params['year']
+    params['month'] = Time.now.month unless params['month']
+    params['country'] = 'US' unless params['country']
+    @months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    @countries = [['US','United States'], ['BE','Belgium'], ['BR','Brazil'], ['GB','Great Britain']]
+    @holidays_user = HolidApi.get(year: params['year'], month: params['month'], country: params['country'])
+    erb :index
+  end
 
-	get '/cats/' do
-		@pictures = CatAPI.new.get_images(category: 'hats')
-		erb :cats
-	end
+  get '/cats/' do
+    @pictures = CatAPI.new.get_images(category: 'hats')
+    erb :cats
+  end
 end
